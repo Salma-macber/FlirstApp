85ab7a4eb4407bb32f93ae0ad690aa75a3f92569
Salma Essam <salma@macber-eg.com>
2025-09-15 11:25:48 +0300

    add new style

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Serve static files from the root directory
+app.use(express.static(__dirname))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
 })
