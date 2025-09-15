02c571d6e6d144608b0c6e50614f26a62e8c3787
Salma Essam <salma@macber-eg.com>
2025-09-15 11:48:11 +0300

    connect to mongo DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
 // Serve static files from the root directory
 app.use(express.static(__dirname))
@@ -12,3 +13,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App listening on port ${port}, open => http://localhost:${port} inside your browser to see the result`)
 })
+// NOTE: and add all-data inside /?
+mongoose.connect('mongodb+srv://salma_db_user:$SalmaAMKBEssam$@cluster0.xl3c3oh.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err)
+    })
+
+
+
+    //mongodb+srv://salma_db_user:$SalmaAMKBEssam$@cluster0.xl3c3oh.mongodb.net/
+    //mongodb+srv://salma_db_user:$SalmaAMKBEssam$@cluster0.xl3c3oh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+    //mongodb+srv://salma_db_user:<db_password>@cluster0.xl3c3oh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
